e01c0eebf3a0acf042c83a9426fd1c5490f6cb92
WU <wu@htlweiz.at>
2016-11-02 08:30:05 +0000

    prerequisites can be installed as any user from any path now

--- terminalsetup.sh
+++ terminalsetup.sh
@@ -1,24 +1,35 @@
-sudo cp /home/pi/sources.list /etc/apt/sources.list
+#!/bin/bash
+
+#upgrade
 sudo apt-get update -y
 sudo apt-get upgrade -y
+
 #CouchDB
 sudo apt-get install -y couchdb
+
+
 #PN532Reader
-sudo cd /home/pi
-sudo mkdir libnfc
-sudo wget https://libnfc.googlecode.com/files/libnfc-1.7.0.tar.bz2
-sudo tar -jxvf libnfc-1.7.0.tar.bz2
-sudo cd libnfc-libnfc-1.7.0
-sudo mkdir /etc/nfc
-sudo mkdir /etc/nfc/devices.d
-sudo cp contrib/libnfc/pn532_uart_on_rpi.conf.sample /etc/nfc/devices.d/pn532_u$
-sudo echo /etc/nfc/devices.d/pn532_uart_on_rpi.config "allow_intrusive_scan = t$
+pushd .
+cd
+mkdir -p libnfc
+wget https://bintray.com/artifact/download/nfc-tools/sources/libnfc-1.7.1.tar.bz2
+tar -jxvf libnfc-1.7.1.tar.bz2
+cd libnfc-1.7.1
+sudo mkdir -p /etc/nfc
+sudo mkdir -p /etc/nfc/devices.d
+TMPFILE=$(tempfile) || exit
+cp contrib/libnfc/pn532_uart_on_rpi.conf.sample "$TMPFILE"
+echo "allow_intrusive_scan = true" >> "$TMPFILE"
+sudo cp "$TMPFILE" /etc/nfc/devices.d/pn532_uart_on_rpi.conf
+rm "$TMPFILE"
 sudo apt-get install -y autoconf libtool
 sudo apt-get install -y libpcsclite-dev libusb-dev
-sudo autoreconf -vis
-sudo ./configure --with-drivers=pn532_uart --sysconfdir=/etc --prefi=/usr
+autoreconf -vis
+./configure --with-drivers=pn532_uart --sysconfdir=/etc --prefi=/usr
 sudo make clean
 sudo make install all
+popd
+
 #mono
 sudo apt-get install -y mono-complete
 #avahi-daemon
@@ -26,17 +37,16 @@ sudo apt-get install -y avahi-daemon
 #Dnssd Lib for Program
 sudo apt-get install -y libavahi-compat-libdnssd-dev
 #set hostname and hostname for avahi
-sudo cp /home/pi/avahi-daemon.conf /etc/avahi/
-cd
+sudo cp avahi-daemon.conf /etc/avahi/
 
-#add TimeRecordingTerminal to autostart
-touch tempbash
-head -n -1 .bashrc tempbash; mv tempbash .bashrc
-crontab -u pi -l | { cat; echo "@reboot sudo mono /home/pi/TimeRecordingTermina$
-#add hostname.sh to autostart on reboot to set hostname to macaddress
-crontab -u pi -l | { cat; echo "@reboot sudo sh /home/pi/hostname.sh &"; } | cr$
+###add TimeRecordingTerminal to autostart
+##touch tempbash
+##head -n -1 .bashrc tempbash; mv tempbash .bashrc
+##crontab -u pi -l | { cat; echo "@reboot sudo mono /home/pi/TimeRecordingTermina$
+###add hostname.sh to autostart on reboot to set hostname to macaddress
+##crontab -u pi -l | { cat; echo "@reboot sudo sh /home/pi/hostname.sh &"; } | cr$
 
-#setup for services
-sudo systemctl enable systemd-networkd-wait-online.service
-sudo systemctl enable timerecordingterminal.service
+###setup for services
+##sudo systemctl enable systemd-networkd-wait-online.service
+##sudo systemctl enable timerecordingterminal.service
 
